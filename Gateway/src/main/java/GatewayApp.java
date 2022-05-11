7b20e858ca7dfa21955eab84847fdaefb7d0b3e5
Daniel Gu <wenyu.gu1998@gmail.com>
2022-05-10 22:10:21 -0500

    fix gateway dep

--- Gateway/src/main/java/GatewayApp.java
+++ Gateway/src/main/java/GatewayApp.java
@@ -7,7 +7,7 @@ import quickfix.*;
 public class GatewayApp {
     public static void main(String[] args) throws ConfigError, FieldConvertError {
 
-        SessionSettings settings = new SessionSettings("./conf/gateway.cfg");
+        SessionSettings settings = new SessionSettings("conf/gateway.cfg");
 
         Application gatewayApplication = new Gateway(settings);
         FileStoreFactory fileStoreFactory = new FileStoreFactory(settings);
