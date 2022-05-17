817fab724eff33893d935b52202df6cf9ec1a1b0
Daniel Gu <wenyu.gu1998@gmail.com>
2022-05-16 22:28:35 -0500

    added gif

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -14,7 +14,7 @@ function trap_ctrlc ()
 if [[ $# -eq 0 ]] || [[ NUM_ME -eq 1 ]]
 then
   echo "Using one matching engine..."
-#  vagrant ssh matching-engine1 -c "cd /vagrant && mvn -pl MatchingEngine -am clean package && java --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens jdk.unsupported/sun.misc=ALL-UNNAMED -jar MatchingEngine/target/MatchingEngine-1.0-SNAPSHOT-jar-with-dependencies.jar 192.168.0.51 1"
+#  vagrant ssh matching-engine1 -c "nohup $(cd /vagrant && mvn -pl MatchingEngine -am clean package && java --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens jdk.unsupported/sun.misc=ALL-UNNAMED -jar MatchingEngine/target/MatchingEngine-1.0-SNAPSHOT-jar-with-dependencies.jar 192.168.0.51 1 > /dev/null 2>&1 &)"
 
 elif [[ NUM_ME -eq 2 ]]
 then
